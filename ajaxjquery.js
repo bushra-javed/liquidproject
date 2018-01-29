aaff0ab1d1afba33ae5e6e12a3ad2ce0a87ffdfa
bushrazafar <35927477+bushrazafar@users.noreply.github.com>
2018-01-29 07:51:42 -0800

    Initial commit

---
+++ ajaxjquery.js
@@ -0,0 +1,85 @@
+﻿$(document).ready(function () {
+    
+            var counter = 1;
+            var outputjson = '';
+            var btnjson = $('#jsondata');
+            btnjson.bind('click', loadjson);
+
+
+            function loadjson() {
+
+                var xhttpjson = new XMLHttpRequest();
+                xhttpjson.open("GET", "peoplejson" + counter + ".json", true);
+                xhttpjson.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            //document.getElementById("demo").innerHTML = xhttp.responseText;
+            console.log(" before parse" + xhttpjson.responseText);
+        var response = JSON.parse(xhttpjson.responseText);
+                        console.log("after parse " +response.people[0].firstname);
+                        var people = response.people;
+                        outputjson += '<table class="table table-bordered table-striped" style="border:2px solid black;">';
+                        outputjson += '<tr style="border:2px solid black;"><th>First Name </th><th>First Name </th><th>age </th><th>street </th><th>state </th></tr>'
+                        datafunction(people);
+
+                    }
+                };
+                xhttpjson.send();
+                counter++;
+                if (counter > 3) {
+                btnjson.hide();
+            };
+
+            };
+            function datafunction(people) {
+                outputjson += '<div style="height:10px;"></div >' +
+                    '<h1>employee  detail</h1></br>'
+                //for (var i = 0; i < people.length; i++)
+                for (var i in  people)
+                {
+                //console.log(people[i]);
+                outputjson += '<tr style="border:1px solid blue;">';
+            outputjson += '<td style="border:1px solid blue;">' + 'first name:     ' + people[i].firstname + '</td>';
+                    outputjson += '<td style="border:1px solid blue;">' + 'last name:       ' + people[i].lastname + ' </td>';
+                    outputjson += '<td style="border:1px solid blue;">' + 'age is :       ' + people[i].age + ' </td>';
+
+                    console.log(people[i].address.street);
+                    outputjson += '<td style="border:1px solid blue;">' + 'adreess street:     ' + people[i].address.street + '</td>';
+                    outputjson += '<td style="border:1px solid blue;">' + 'adreess street:     ' + people[i].address.state + '</td>';
+                     outputjson += '</tr>';
+                }
+                //alert(data);
+                outputjson += '</table>'
+    outputjson += '</br>'
+
+    $('#JSON_LOCAL').html(outputjson);
+
+}
+
+
+       
+    //$('#content').load('about.html');
+
+    //$('.mainnavbar ul.navbar-nav div.dropdown-menu a.dropdown-item').click(function () {
+    //    var page = $(this).attr('href');
+    //       //$('#content').load('index.html');
+    //    alert($(this));
+    //    alert(page);
+    //    $('#content').load(page + '.html');
+    //    return false;
+    //});
+
+
+    //Create an index.html, specify a < div class="container" > and leave this empty.Then use jQuery to load home.html into the .container object, and do the same for all other pages.
+
+    //    $(document).ready(function () {
+    //        $(".container").load("home.html");
+    //    });
+
+    //$("ul.navbar-nav li").each(function () {
+    //    $(this).on("click", function{
+    //        $(".container").load(($this).attr("data-page") + ".html");
+    //    });
+    //});
+    //In this case, the href value of your URL should always be "#" and you should give it a data- page="about" if you want it to show the about page.
+});
---
+++ angular-practice/angular-script - Copy (3).js
@@ -0,0 +1,275 @@
+﻿////<reference path="Scripts/angular.min.js"/>
+//method 1
+var myApp = angular.module("myModule", []);
+var myController = function ($scope) {
+    var employee = {
+        firstname: "bushra",
+        lastname: "javed",
+        age:"30"
+    }
+
+    $scope.employee=employee; 
+
+  $scope.message = "this is message ";
+
+
+};
+myApp.controller('myController', myController);
+//method 2
+//or do this
+
+
+//myApp.controller('myController', function ($scope) {
+//    $scope.message = "bushra angular";
+//});
+
+
+//method 3
+
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController",  function ($scope) {
+//    var employee = {
+//        firstname: "bushra",
+//        lastname: "javed",
+//        age: "30"
+//    }
+
+//    $scope.employee = employee;
+
+
+
+//});
+
+// tutorial 4 image ng-src
+
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+//        var country = {
+//            name: "USA",
+//            capital: "w.DC",
+//            flag: "images/1.jpg"
+//        };
+//        $scope.country = country;
+
+//    });
+
+
+// tutorial 5
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+//        var employee = {
+//        firstname: "abushra",
+//        lastname: "javed",
+//        age: "30"
+//    }
+
+//    $scope.employee = employee;
+//    $scope.message = "hello this is message";
+//    });
+            
+// tutorial 6 part1
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController",  function ($scope) {
+//        var employees =[
+//            { firstname: "naheed", lastname: "zafar", gender: "female", salary: 550000 },
+//            { firstname: "shahnaz", lastname: "mazhar", gender: "female", salary: 600000 },
+//            { firstname: "samra", lastname: "shahid", gender: "female", salary: 700000},
+//            { firstname: "rukhsana", lastname: "yasir", gender: "female", salary: 700000 },
+//            { firstname: "bushra", lastname: "zafar", gender: "female", salary: 550000},
+//            { firstname: "navee", lastname: "javed", gender: "male", salary: 600000},
+//            { firstname: "qadeer", lastname: "zafar", gender: "male", salary: 500000 }
+//        ]
+//    $scope.employees = employees;
+
+
+
+//});
+
+
+
+// tutorial 6 part2
+
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+//        var countries = [
+//            {
+//                name: "USA",
+//                cities: [
+//                    { name: "new jersey" },
+//                    { name: "Manchester" },
+//                    { name: "Birmingham" }
+//                ]
+//            },
+//            {
+//                name: "USA",
+//                cities: [
+//                    { name: "new jersey" },
+//                    { name: "Manchester" },
+//                    { name: "Birmingham" }
+//                ]
+//            },
+//            {
+//                name: "USA",
+//                cities: [
+//                    { name: "new jersey" },
+//                    { name: "Manchester" },
+//                    { name: "Birmingham" }
+//                ]
+//            }
+//        ];
+//        $scope.countries = countries;
+
+//    });
+
+
+
+
+// tutorial 7
+
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+//        var technologies = [
+//            { name: "c#", likes: 0, dislikes: 0 },
+//            { name: "asp.net", likes: 0, dislikes: 0 },
+//            { name: "sql server", likes: 0, dislikes: 0 },
+//            { name: "angular JS", likes: 0, dislikes: 0 },
+//            { name: "html", likes: 0, dislikes: 0 }
+//        ];
+//        $scope.technologies = technologies;
+//        $scope.incrementlikes = function (technology) {
+//            technology.likes++;
+//        }
+//        $scope.incrementdislikes = function (technology) {
+//            technology.dislikes++;
+//        }
+        
+//    });
+
+
+// tutorial 8
+//var myApp = angular
+    //.module("myModule", [])
+    //.controller("myController",  function ($scope) {
+    //    var employees =[
+    //        { firstname: "naheed", lastname: "zafar", dob: new Date("November 23,1980"), gender: "female", salary: 550000 },
+    //        { firstname: "shahnaz", lastname: "mazhar", dob: new Date("November 23,1979"), gender: "female", salary: 600000 },
+    //        { firstname: "samra", lastname: "shahid", dob: new Date("November 23,1970"), gender: "female", salary: 700000},
+    //        { firstname: "rukhsana", lastname: "yasir", dob: new Date("November 23,1977"), gender: "female", salary: 700000 },
+    //        { firstname: "bushra", lastname: "zafar", dob: new Date("November 23,1981"), gender: "female", salary: 550000},
+    //        { firstname: "navee", lastname: "javed", dob: new Date("November 23,1983"), gender: "male", salary: 600000},
+    //        { firstname: "qadeer", lastname: "zafar", dob: new Date("November 23,1984"), gender: "male", salary: 500000 }
+
+    //    ]
+
+    //    $scope.employees = employees;
+    //    $scope.rowlimit = 3;
+
+    //});
+
+ //tutorial 9
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController",  function ($scope) {
+//        var employees =[
+//            { firstname: "naheed", lastname: "zafar", dob: new Date("November 23,1980"), gender: "female", salary: 550000 },
+//            { firstname: "shahnaz", lastname: "mazhar", dob: new Date("November 23,1979"), gender: "female", salary: 600000 },
+//            { firstname: "samra", lastname: "shahid", dob: new Date("November 23,1970"), gender: "female", salary: 700000},
+//            { firstname: "rukhsana", lastname: "yasir", dob: new Date("November 23,1977"), gender: "female", salary: 700000 },
+//            { firstname: "bushra", lastname: "zafar", dob: new Date("November 23,1981"), gender: "female", salary: 550000},
+//            { firstname: "navee", lastname: "javed", dob: new Date("November 23,1983"), gender: "male", salary: 600000},
+//            { firstname: "qadeer", lastname: "zafar", dob: new Date("November 23,1984"), gender: "male", salary: 500000 }
+
+//        ]
+
+//        $scope.employees = employees;
+//        $scope.sortcolumn = "firstname";
+
+//    });
+
+
+//tutorial 10
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+//        var employees = [
+//            { firstname: "naheed", lastname: "zafar", dob: new Date("November 23,1980"), gender: "female", salary: 550000 },
+//            { firstname: "shahnaz", lastname: "mazhar", dob: new Date("November 23,1979"), gender: "female", salary: 600000 },
+//            { firstname: "samra", lastname: "shahid", dob: new Date("November 23,1970"), gender: "female", salary: 700000 },
+//            { firstname: "rukhsana", lastname: "yasir", dob: new Date("November 23,1977"), gender: "female", salary: 700000 },
+//            { firstname: "bushra", lastname: "zafar", dob: new Date("November 23,1981"), gender: "female", salary: 550000 },
+//            { firstname: "navee", lastname: "javed", dob: new Date("November 23,1983"), gender: "male", salary: 600000 },
+//            { firstname: "qadeer", lastname: "zafar", dob: new Date("November 23,1984"), gender: "male", salary: 500000 }
+
+//        ]
+//        $scope.employees = employees;
+//        $scope.sortcolumn = "firstname";
+//        $scope.reversesort = false;
+//        $scope.sortData = function (column) {
+//            $scope.reversesort = ($scope.sortcolumn == column) ? !$scope.reversesort : false;
+//            $scope.sortcolumn = column;
+
+//        }
+//        $scope.getsortclass = function (column) {
+//            if ($scope.sortcolumn == column) {
+//                return $scope.reversesort ? 'arrow-down' : 'arrow-up'
+//            }
+//            return '';
+//        }
+
+//    });
+
+//tutorial 11 12a
+//var myApp = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+//        var employees = [
+//            { firstname: "naheed", lastname: "zafar", dob: new Date("November 23,1980"), gender: "female", salary: 550000 },
+//            { firstname: "shahnaz", lastname: "mazhar", dob: new Date("November 23,1979"), gender: "female", salary: 600000 },
+//            { firstname: "samra", lastname: "shahid", dob: new Date("November 23,1970"), gender: "female", salary: 700000 },
+//            { firstname: "rukhsana", lastname: "yasir", dob: new Date("November 23,1977"), gender: "female", salary: 700000 },
+//            { firstname: "bushra", lastname: "zafar", dob: new Date("November 23,1981"), gender: "female", salary: 550000 },
+//            { firstname: "navee", lastname: "javed", dob: new Date("November 23,1983"), gender: "male", salary: 600000 },
+//            { firstname: "qadeer", lastname: "zafar", dob: new Date("November 23,1984"), gender: "male", salary: 500000 }
+
+//        ]
+//        $scope.employees = employees;
+//    });
+
+//tutorial 12b
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+        var employees = [
+            { firstname: "naheed", lastname: "zafar", dob: new Date("November 23,1980"), gender: "female", salary: 550000 },
+            { firstname: "shahnaz", lastname: "mazhar", dob: new Date("November 23,1979"), gender: "female", salary: 600000 },
+            { firstname: "samra", lastname: "shahid", dob: new Date("November 23,1970"), gender: "female", salary: 700000 },
+            { firstname: "rukhsana", lastname: "yasir", dob: new Date("November 23,1977"), gender: "female", salary: 700000 },
+            { firstname: "bushra", lastname: "zafar", dob: new Date("November 23,1981"), gender: "female", salary: 550000 },
+            { firstname: "navee", lastname: "javed", dob: new Date("November 23,1983"), gender: "male", salary: 600000 },
+            { firstname: "qadeer", lastname: "zafar", dob: new Date("November 23,1984"), gender: "male", salary: 500000 }
+
+        ]
+        $scope.employees = employees;
+        $scope.search = function (item) {
+            if ($scope.searchtext == undefined) {
+                return true;
+            }
+            else {
+                if (item.firstname.toLowerCase().indexOf($scope.searchtext.toLowerCase()) != -1 ||
+                    item.lastname.toLowerCase().indexOf($scope.searchtext.toLowerCase())!= -1 
+                ) {
+                    return true;
+                }
+                    
+            }
+            return false;
+        }
+
+    });
+
